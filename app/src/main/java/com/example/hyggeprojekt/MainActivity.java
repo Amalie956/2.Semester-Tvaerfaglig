700d7faf7ccd95a60fbf2b59cab1f9c61300492e
Amalie956 <amal956e@stud.kea.dk>
2023-04-17 17:15:26 +0200

    Ændret farver på ActionBar

--- app/src/main/java/com/example/hyggeprojekt/MainActivity.java
+++ app/src/main/java/com/example/hyggeprojekt/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hyggeprojekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
