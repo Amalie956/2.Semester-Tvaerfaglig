b5ab56ad7995a59179cb0d3091d44a78b01485b9
Amalie956 <amal956e@stud.kea.dk>
2023-04-18 11:06:03 +0200

    Read data fra firestor. Se TivoligActivity

--- app/src/main/java/com/example/hyggeprojekt/TivoliActivity.java
+++ app/src/main/java/com/example/hyggeprojekt/TivoliActivity.java
@@ -1,5 +1,6 @@
 package com.example.hyggeprojekt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,26 @@ public class TivoliActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tivoli_activity);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("tivoli")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                System.out.println(document.getData());
+                                System.out.println("document.getData()");
+                                System.out.println(document.getData());
+                                System.out.println(document.getData());
+                                System.out.println(document.getData());
+                            }
+                        } else {
+                            System.out.println(task.getException());
+                        }
+                    }
+                });
     }
 }
